f0ae39dc57b6f10443c5e19bb6aa2241c0d24876
Victormattos564 <victormaria@aluno.senai.br>
2024-03-11 14:46:55 -0300

    Adiciona estilos e componente de título à tela inicial

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
-import React from 'react'
 
+import Title from '../../components/Title'
+import styles from './styles'
 export default function Home() {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Title title={"Home"} />
     </View>
   )
 }
