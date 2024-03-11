9a9910736df2a6ab744f9088217a3cb75d7ac9bf
Victormattos564 <victormaria@aluno.senai.br>
2024-03-11 14:40:21 -0300

    feat: cretae title components

---
+++ src/components/Title/styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+});
+
+export default styles;
